66d16c48ed9bc6ebdd174c0ae340d1aa9bbc33e1
ShellBakerIO <shellbaker123@gmail.com>
2024-01-14 02:32:48 +0500

    first commit

---
+++ main/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Home, Demand, Skills, Geography
+
+
+
+@admin.register(Home)
+class HomePage(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(Demand)
+class HomePage(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(Geography)
+class HomePage(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+@admin.register(Skills)
+class HomePage(admin.ModelAdmin):
+    list_display = ('title',)
